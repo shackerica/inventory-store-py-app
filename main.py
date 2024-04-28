3dcc3dc8f6f702b9a2ae2acd5da98b12254e650a
Erica Shackelford <ericalshackelford@gmail.com>
2024-04-28 14:03:20 +0000

    Add product details to inventory dictionary
    - Inserted "Product1" name and related details into the inventory
    - Added product quantity, price, and release year

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ Product1= "Mobile Phone"
 Product1_quantity = 5
 Product1_price = 20000
 Product1_releaseYear = 2020
+
+# Add details in inventory
+inventory["Product1"] = Product1
+inventory["Product1_quantity"] = Product1_quantity
+inventory["Product1_price"] = Product1_price
+inventory["Product1_releaseYear"] = Product1_releaseYear
+
