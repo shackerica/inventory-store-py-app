b0da894b30f1318ab2128303aac9ef5d43d6002e
Erica Shackelford <ericalshackelford@gmail.com>
2024-04-28 13:55:52 +0000

    Created variables for the product details

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # Empty dictionary to store product details
 inventory = {}
+
+# Variables for product details 
+Product1= "Mobile Phone"
+Product1_quantity = 5
+Product1_price = 20000
+Product1_releaseYear = 2020
