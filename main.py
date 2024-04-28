e6586030eadc317ed6f6184b16024d9869efef47
Erica Shackelford <ericalshackelford@gmail.com>
2024-04-28 14:08:14 +0000

    Created variables for the 2nd product details

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Empty dictionary to store product details
 inventory = {}
 
-# Variables for product details 
-Product1= "Mobile Phone"
+# Variables for product details
+Product1 = "Mobile Phone"
 Product1_quantity = 5
 Product1_price = 20000
 Product1_releaseYear = 2020
@@ -13,3 +13,8 @@ inventory["Product1_quantity"] = Product1_quantity
 inventory["Product1_price"] = Product1_price
 inventory["Product1_releaseYear"] = Product1_releaseYear
 
+# Variables for the 2nd proudct details
+Product2 = "Laptop"
+Product2_quantity = 10
+Product2_price = 50_0000
+Product2_releaseYear = 2020
