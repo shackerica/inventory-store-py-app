270c9e5377557bdaadbf77006800387c2df8c1da
Erica Shackelford <ericalshackelford@gmail.com>
2024-04-28 14:12:09 +0000

    Add 2nd product details to the inventory dictionary

--- main.py
+++ main.py
@@ -13,8 +13,14 @@ inventory["Product1_quantity"] = Product1_quantity
 inventory["Product1_price"] = Product1_price
 inventory["Product1_releaseYear"] = Product1_releaseYear
 
-# Variables for the 2nd proudct details
+# Variables for the 2nd product details
 Product2 = "Laptop"
 Product2_quantity = 10
 Product2_price = 50_0000
 Product2_releaseYear = 2020
+
+# Add 2nd product details in inventory
+inventory["Product2"] = Product2
+inventory["Product2_quantity"] = Product2_quantity
+inventory["Product2_price"] = Product2_price
+inventory["Product2_releaseYear"] = Product2_releaseYear
